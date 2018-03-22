96e868da5e3e04bbdffae9851c7a9f3f6c2e84a9
niawjunior <pasupol@dome.cloud>
2018-03-22 13:27:34 +0700

    Composing with components

--- Introdutions/app.js
+++ Introdutions/app.js
@@ -55,3 +55,19 @@
       message: 'hello world'
     }
   })
+
+  Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+  })
+  
+  var app7 = new Vue({
+    el: '#app7',
+    data: {
+      List: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+  })
